e02abcd8ee554f9cbbfd1d5b3cd800a4a67aaa6f
Andrii-888 <curupa8888@gmail.com>
2023-01-20 16:27:58 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String str = new String("I study Basic Java!" );
+        Main.printString(str);
+        System.out.println(str.charAt(str.length() - 1));
+
+        System.out.println(str.contains("Java"));
+        System.out.println(str.replace("a", "o"));
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+        System.out.println(str.substring(14,18));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public static void printString(String s)
+    {
+        // logic
+    }
+}
